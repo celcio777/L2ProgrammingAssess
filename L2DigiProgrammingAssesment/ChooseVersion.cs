9e154d76d6371ad636f5b2b3aa6ae74be23f47b8
nick cooper <nicknack359@gmail.com>
2020-07-26 14:58:15 +1200

    Updated to Initial Entry

---
+++ L2DigiProgrammingAssesment/ChooseVersion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L2DigiProgrammingAssesment
+{
+    class ChooseVersion
+    {
+        static List<Action> programVersions = new List<Action>();
+        static string[] versionNames = new string[] { "Input", "ChangeIngreds" };
+        static int[] actualVersion = new int[] { 1, 2 };
+        static string versionToRun;
+        static int parsedVersion;
+
+        public static void Main(string[] args)
+        {
+            programVersions.Add(() => V1.V1Class());
+            Console.WriteLine("<This is for version testing only>");
+            Console.WriteLine("Which version would you like to run?");
+            for (int i = 0; i < actualVersion.Length; i++)
+            {
+                Console.WriteLine($"V{actualVersion[i]}: {versionNames[i]}");
+            }
+
+            versionToRun = Console.ReadLine();
+            // taking the input and converting it to only numbers so it can be parsed.
+            if (versionToRun.Length > 0)
+            {
+                versionToRun = new string(versionToRun.Where(c => Char.IsDigit(c) || c == '.' || c == ',').ToArray());
+            }
+            // parsing the string into a float before int in case of decimals
+            float decimals = float.Parse(versionToRun);
+            if (decimals > 0 && decimals < actualVersion.Max())
+            {
+                // rounding float to 0dp so it can go into integer
+                parsedVersion = (int)Math.Round(decimals - 1, 0);
+            }
+
+
+            Console.WriteLine($"Now running version: {versionToRun}");
+            programVersions[parsedVersion].Invoke();
+        }
+    }
+}
---
+++ L2DigiProgrammingAssesment/Version_01.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L2DigiProgrammingAssesment
+{
+    class V1
+    {
+        static string name1, name2;
+        static float price1, price2, size1, size2;
+        public static void V1Class()
+        {
+            Console.WriteLine("What are the Name, Price, and size of your products?");
+            Console.WriteLine("1st Name:");
+            name1 = Console.ReadLine();
+            Console.WriteLine("1st Price:");
+            price1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter all sizes in the same units");
+            Console.WriteLine("1st Size:");
+            size1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Next Product:");
+            Console.WriteLine("2nd Name:");
+            Console.WriteLine("2nd Price:"); 
+            Console.WriteLine("2nd Size:");
+
+        }
+    }
+}
