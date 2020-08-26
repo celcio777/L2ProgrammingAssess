49e99396cfba9235ac1b7a212be3f9f2cfe0fc3f
Nick Cooper <nicknack359@gmail.com>
2020-08-26 13:16:24 +1200

    V3 Complete

--- L2DigiProgrammingAssesment/ChooseVersion.cs
+++ L2DigiProgrammingAssesment/ChooseVersion.cs
@@ -8,14 +8,15 @@ namespace L2DigiProgrammingAssesment
     class ChooseVersion
     {
         static List<Action> programVersions = new List<Action>();
-        static string[] versionNames = new string[] { "Basic functionality"};
-        static int[] actualVersion = new int[] { 1};
+        static string[] versionNames = new string[] { "Basic functionality", "Display improvements", "Expanded functionality"};
+        static int[] actualVersion = new int[] { 1,2,3};
         static string versionToRun;
 
         public static void Main(string[] args)
         {
             programVersions.Add(() => V1.V1Main());
             programVersions.Add(() => V2.V2Main());
+            programVersions.Add(() => V3.V3Main());
             Console.WriteLine("<This is for version testing only>");
             Console.WriteLine("Which version would you like to run?");
             for (int i = 0; i < actualVersion.Length; i++)
